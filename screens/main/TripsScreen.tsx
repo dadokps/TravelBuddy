9a38719d097e8a3fe1a3be764350f562c1b87bb5
Damir Popovic <Damir.Popovic@wbstraining.de>
2025-03-30 22:07:02 +0200

    Add Screens

---
+++ screens/main/TripsScreen.tsx
@@ -0,0 +1,13 @@
+// PublishScreen.tsx
+import React from 'react';
+import { Box, Text } from 'native-base';
+
+export const TripsScreen = () => {
+  return (
+    <Box flex={1} alignItems="center" justifyContent="center">
+      <Text>Trips Screen</Text>
+    </Box>
+  );
+};
+
+// Similarly create TripsScreen.tsx, HistoryScreen.tsx, ProfileScreen.tsx
